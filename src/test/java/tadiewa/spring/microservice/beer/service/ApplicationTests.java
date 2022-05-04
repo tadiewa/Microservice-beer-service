ff0006b2f8d692ca5dcadcb2d02124a4a15ba17a
tadiewa <tadiwanashev@afrosoft.co.zw>
2022-05-04 11:36:09 +0200

    intial project add . Closes #1

---
+++ src/test/java/tadiewa/spring/microservice/beer/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tadiewa.spring.microservice.beer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
