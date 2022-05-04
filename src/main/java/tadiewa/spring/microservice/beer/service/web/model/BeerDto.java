11f96252d38a70ca4445d153ddb631f692f80d10
tadiewa <tadiwanashev@afrosoft.co.zw>
2022-05-04 12:10:28 +0200

    adding web data model. Closes #2

---
+++ src/main/java/tadiewa/spring/microservice/beer/service/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package tadiewa.spring.microservice.beer.service.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyleEnum;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/tadiewa/spring/microservice/beer/service/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package tadiewa.spring.microservice.beer.service.web.model;
+
+public enum BeerStyleEnum {
+    LAGER , PILSNER, STOUT ,GOSE ,PORTER ,ALE ,WHEAT ,IPA ,PALE_ALE ,SAISON
+}
